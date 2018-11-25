f98b93199ef0d182f23c1925cd12f1f3c88a80b0
jchuahtacc <jchuah@tacc.utexas.edu>
2018-11-24 20:20:57 -0600

    Removed excess components

--- src/app/components/xwing.module.ts
+++ src/app/components/xwing.module.ts
@@ -7,20 +7,12 @@ import { XwingShipComponent } from './xwing-ship/xwing-ship.component';
 import { XwingTextComponent } from './xwing-text/xwing-text.component';
 import { PilotComponent } from './pilot/pilot.component';
 import { UpgradeComponent } from './upgrade/upgrade.component';
-import { ChargeSummaryComponent } from './charge-summary/charge-summary.component';
-import { AttackStatsComponent} from './attack-stats/attack-stats.component';
 import { ActionComponent } from './action/action.component';
-import { ForceSummaryComponent } from './force-summary/force-summary.component';
-import { BonusStatsComponent } from './bonus-stats/bonus-stats.component';
-import { HullSummaryComponent} from './hull-summary/hull-summary.component';
-import { ShieldSummaryComponent } from './shield-summary/shield-summary.component';
 import { TokenDisplayComponent } from './token-display/token-display.component';
-import { ForceBonusComponent } from './force-bonus/force-bonus.component';
 import { DamageCardComponent } from './damage-card/damage-card.component';
-import { DamageSummaryComponent} from './damage-summary/damage-summary.component';
-import { ConditionSummaryComponent } from './condition-summary/condition-summary.component';
 import { ConditionComponent } from './condition/condition.component';
 import { StatComponent } from './stat/stat.component';
+import { BonusStatsComponent } from './bonus-stats/bonus-stats.component';
 @NgModule({
   entryComponents: [
   ],
@@ -34,20 +26,12 @@ import { StatComponent } from './stat/stat.component';
     XwingTextComponent, 
     PilotComponent,
     UpgradeComponent,
-    ChargeSummaryComponent,
-    AttackStatsComponent,
     ActionComponent,
-    ForceSummaryComponent,
-    BonusStatsComponent,
-    HullSummaryComponent,
-    ShieldSummaryComponent,
     TokenDisplayComponent,
-    ForceBonusComponent,
     DamageCardComponent,
-    DamageSummaryComponent,
-    ConditionSummaryComponent,
     ConditionComponent,
-    StatComponent
+    StatComponent,
+    BonusStatsComponent
   ],
   exports: [
     XwingIconComponent, 
@@ -55,20 +39,12 @@ import { StatComponent } from './stat/stat.component';
     XwingTextComponent, 
     PilotComponent, 
     UpgradeComponent,
-    ChargeSummaryComponent,
-    AttackStatsComponent,
     ActionComponent,
-    ForceSummaryComponent,
-    BonusStatsComponent,
-    HullSummaryComponent,
-    ShieldSummaryComponent,
     TokenDisplayComponent,
-    ForceBonusComponent,
     DamageCardComponent,
-    DamageSummaryComponent,
-    ConditionSummaryComponent,
     ConditionComponent,
-    StatComponent
+    StatComponent,
+    BonusStatsComponent
   ]
 })
 export class XwingModule {}
